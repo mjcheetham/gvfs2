1a87c6e4d4c79e21a3d6911148636ff85d85be4a
Matthew John Cheetham <mjcheetham@outlook.com>
2021-06-18 14:07:52 +0100

    WIP102

--- GVFS/GVFS.Installers/install.bat
+++ GVFS/GVFS.Installers/install.bat
@@ -5,8 +5,15 @@ REM Lookup full paths to Git and VFS for Git installers
 FOR /F "tokens=* USEBACKQ" %%F IN ( `where /R %~dp0 Git*.exe` ) DO SET GIT_INSTALLER=%%F
 FOR /F "tokens=* USEBACKQ" %%F IN ( `where /R %~dp0 SetupGVFS*.exe` ) DO SET GVFS_INSTALLER=%%F
 
+REM Create new empty directory for logs
+SET LOGDIR=%~dp0\logs
+IF EXIST %LOGDIR% (
+    rmdir /S /Q %LOGDIR%
+)
+mkdir %LOGDIR%
+
 ECHO Installing Git for Windows...
-CALL %GIT_INSTALLER% /DIR"C:\Program Files\Git" /NOICONS /COMPONENTS="ext,ext\shellhere,ext\guihere,assoc,assoc_sh" /GROUP="Git" /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /ALLOWDOWNGRADE=1
+%GIT_INSTALLER% /LOG="%LOGDIR%\git.log" /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /ALLOWDOWNGRADE=1
 
 ECHO Installing VFS for Git...
-CALL %GVFS_INSTALLER% /VERYSILENT /SUPPRESSMSGBOXES /NORESTART
+%GVFS_INSTALLER% /LOG="%LOGDIR%\gvfs.log" /VERYSILENT /SUPPRESSMSGBOXES /NORESTART
