986f33832cfd1afafac439684ea91f539d36ab86
Matthew John Cheetham <mjcheetham@outlook.com>
2021-06-23 16:45:41 +0100

    WIP10X

--- GVFS/GVFS.FunctionalTests/Tests/EnlistmentPerTestCase/LooseObjectStepTests.cs
+++ GVFS/GVFS.FunctionalTests/Tests/EnlistmentPerTestCase/LooseObjectStepTests.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System;
 
 namespace GVFS.FunctionalTests.Tests.EnlistmentPerTestCase
 {
--- GVFS/GVFS.FunctionalTests/Tools/ProcessHelper.cs
+++ GVFS/GVFS.FunctionalTests/Tools/ProcessHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using System.IO;
+using System;
 
 namespace GVFS.FunctionalTests.Tools
 {
